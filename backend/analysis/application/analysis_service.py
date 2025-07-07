46a1c95af146c85cb6edb9f658d07bcbe50af837
김희수 <wlslaka4@gmail.com>
2025-07-07 20:44:49 +0900

    [add] cli와 tradingagents 모듈 위치 변경

--- backend/analysis/application/analysis_service.py
+++ backend/analysis/application/analysis_service.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
-
 import logging
 from sqlmodel import Session
 from analysis.domain.repository.analysis_repo import IAnalysisRepository
