75924a2b94f492fc3035d2bd2c67ba816e66f43a
Jenit Jain <jenitjai@student.ubc.ca>
2025-08-05 22:06:50 -0700

    Run the basic workflow

--- tradingagents/agents/utils/memory.py
+++ tradingagents/agents/utils/memory.py
@@ -11,7 +11,13 @@ class FinancialSituationMemory:
             self.embedding = "text-embedding-3-small"
         self.client = OpenAI(base_url=config["backend_url"])
         self.chroma_client = chromadb.Client(Settings(allow_reset=True))
-        self.situation_collection = self.chroma_client.create_collection(name=name)
+        # Check if collection exists
+        try:
+            self.situation_collection = self.chroma_client.get_collection(name=name)
+        except:
+            # Collection doesn't exist, create it
+            self.situation_collection = self.chroma_client.create_collection(name=name)
+        # self.situation_collection = self.chroma_client.create_collection(name=name)
 
     def get_embedding(self, text):
         """Get OpenAI embedding for a text"""
